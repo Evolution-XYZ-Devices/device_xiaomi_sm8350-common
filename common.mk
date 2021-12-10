2dc565f74236687d2291ec81f0e4765bd2926df3
Dyneteve <dyneteve@hentaios.com>
2021-12-10 13:53:25 +0000

    sm8350-common: remove Renderscript & and move renderengine to threaded skia
    This has been deprecated in A12
    
    https://developer.android.com/guide/topics/renderscript/migrate
    
    Signed-off-by: Omkar Chandorkar <gotenksIN@aosip.dev>
    Change-Id: I9f303ed996144f50a6b7fb8abb02b4eb11e6e075

--- common.mk
+++ common.mk
@@ -438,10 +438,6 @@ PRODUCT_PACKAGES += \
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/rootdir/etc/init.recovery.qcom.rc:$(TARGET_COPY_OUT_RECOVERY)/root/init.recovery.qcom.rc
 
-# RenderScript HAL
-PRODUCT_PACKAGES += \
-    android.hardware.renderscript@1.0-impl
-
 # RIL
 PRODUCT_PACKAGES += \
     CarrierConfigOverlay \
