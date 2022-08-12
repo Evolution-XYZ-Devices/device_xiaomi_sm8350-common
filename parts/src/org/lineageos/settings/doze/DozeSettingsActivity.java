3ad1f290d86974427557d1768e1de19e22af689b
Joey Huab <joey@evolution-x.org>
2022-08-12 14:22:04 +0000

    sm8350-common: parts: Adapt to S style

--- parts/src/org/lineageos/settings/doze/DozeSettingsActivity.java
+++ parts/src/org/lineageos/settings/doze/DozeSettingsActivity.java
@@ -29,9 +29,7 @@ public class DozeSettingsActivity extends CollapsingToolbarBaseActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
-        getFragmentManager()
-                .beginTransaction()
-                .replace(R.id.content_frame, new DozeSettingsFragment(), TAG_DOZE)
-                .commit();
+        getFragmentManager().beginTransaction().replace(R.id.content_frame,
+                new DozeSettingsFragment(), TAG_DOZE).commit();
     }
 }
--- parts/src/org/lineageos/settings/thermal/ThermalActivity.java
+++ parts/src/org/lineageos/settings/thermal/ThermalActivity.java
@@ -17,7 +17,6 @@
 package org.lineageos.settings.thermal;
 
 import android.os.Bundle;
-import android.view.MenuItem;
 
 import com.android.settingslib.collapsingtoolbar.CollapsingToolbarBaseActivity;
 import com.android.settingslib.widget.R;
@@ -31,14 +30,5 @@ public class ThermalActivity extends CollapsingToolbarBaseActivity {
 
          getFragmentManager().beginTransaction().replace(R.id.content_frame,
                 new ThermalSettingsFragment(), TAG_THERMAL).commit();
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        if (item.getItemId() == android.R.id.home) {
-            onBackPressed();
-            return true;
-        }
-        return false;
     }
 }
