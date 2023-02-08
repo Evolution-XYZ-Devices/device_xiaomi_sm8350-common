997a7cf8de28a11095c1c91c4d7ad46ed7a6e067
Alexander Koskovich <zvnexus@outlook.com>
2023-02-08 21:54:13 +0800

    sm8350-common: Update camera provider service to 2.5
    * QTI camera device interface is a no-op with this
    
    Change-Id: Id13db7bbd2d0ee1bd544e1670990c3caf46768ab
    Signed-off-by: ralph950412 <ralph950412@gmail.com>
    Signed-off-by: saikiran2001 <bjsaikiran@gmail.com>

--- common.mk
+++ common.mk
@@ -124,14 +124,10 @@ PRODUCT_PACKAGES_DEBUG += \
 
 # Camera
 PRODUCT_PACKAGES += \
+    android.hardware.camera.provider@2.5-service_64 \
+    vendor.qti.hardware.camera.postproc@1.0.vendor \
+    libcamera2ndk_vendor \
     libgui_vendor \
-    Snap
-
-PRODUCT_PACKAGES += \
-    android.hardware.camera.provider@2.4-impl \
-    android.hardware.camera.provider@2.4-service_64 \
-    vendor.qti.hardware.camera.device@1.0.vendor \
-    vendor.qti.hardware.camera.postproc@1.0.vendor
 
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/camera_cnf.txt:$(TARGET_COPY_OUT_VENDOR)/etc/camera/camera_cnf.txt
