1046dfa7d13a0eb5cb0984d302ceaae9a6a2a433
kamikaonashi <thorschi93@gmx.at>
2023-07-11 06:30:50 +0200

    [SQUASH] sm8350-common: fastcharge implementation
    this commit contains following commits to introduce the fastcharge toggle for sm8350-common devices that use this tree
    
    they were renamed from sm8250-common to sm8350-common
    
     sm8250-common: add fastcharge implementation
    
     * Disabling fast charge may be useful for reducing the heat produced by
       the device while charging, or for extending the lifespan of the battery
    
     * This commit introduces the fastcharge HIDL, which writes in the node
    
                  /sys/class/qcom-battery/restrict_chg
    
       0 or 1 depeding on user selection.
       0 means that fastcharge is enabled, while 1 means that fastcharge is
         disabled
    
    Change-Id: I369ba9c437b3a83a88c2ce74d603b7d7ddd9cfbb
    Signed-off-by: Davide Garberi <dade.garberi@gmail.com>
    Signed-off-by: althafvly <althafvly@gmail.com>
    
    https://github.com/kamikaonashi/device_xiaomi_sm8250-common/commit/6510782439cedeb20fd75c99f264d44e5869b086
    
     sm8250-common: fastcharge: Make user setting persistent
    
    Change-Id: I4ff051d6f9685e94022eab32035bfe86a65264b8
    Signed-off-by: Davide Garberi <dade.garberi@gmail.com>
    
    https://github.com/kamikaonashi/device_xiaomi_sm8250-common/commit/e050f0dab00d0ec22d619e180f1fee2aa5ebc984
    
     sm8250-common: fastcharge: Use pragma once
    
    Change-Id: I9a9e193a6f66f045f41394d059af8b71778e8d72
    
    https://github.com/kamikaonashi/device_xiaomi_sm8250-common/commit/ff8e5f6173f613a8f28559adf4d652ef0348d64b
    
     sm8250-common: fastcharge: Migrate to blueprint
    
    Change-Id: I200fd6c685183c98f0815b207adae5aa75a37238
    
    https://github.com/kamikaonashi/device_xiaomi_sm8250-common/commit/4e0d22f4863bde7ef6c68b54e345374e650b542f
    
    sm8250-common: Build fastcharge implementation
    Change-Id: I3e6994d2c2f419dc8ce98a060204fda3dd16df81
    
    https://github.com/kamikaonashi/device_xiaomi_sm8250-common/commit/136d369c4ef13c52fe4cecb3bbda45df8b6eee4d
    
    sm8250-common: Sepolicy: Label FastCharge more general
    
    https://github.com/kamikaonashi/device_xiaomi_sm8250-common/commit/7a619945ca3b9bf284a1cfdd4063b864bf5e5290
    
    sm8250-common: Sepolicy: Fastcharge: Provide needed permissions
    
    https: //github.com/kamikaonashi/device_xiaomi_sm8250-common/commit/298ff28a2ab9c04f953780958de6a8971a947c51
    Co-Authored-By: Ivan Martynov <johnivan19999@gmail.com>
    Co-Authored-By: Francescodario Cuzzocrea <bosconovic@gmail.com>
    Co-Authored-By: Bruno Martins <1696148+bgcngm@users.noreply.github.com>
    Co-Authored-By: Davide Garberi <dade.garberi@gmail.com>
    Co-Authored-By: Mohammed Althaf Thayyil <17639653+althafvly@users.noreply.github.com>

--- common.mk
+++ common.mk
@@ -194,6 +194,10 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     fastbootd
 
+# Fastcharge
+PRODUCT_PACKAGES += \
+    vendor.lineage.fastcharge@1.0-service.xiaomi_sm8350
+
 # Fingerprint
 PRODUCT_PACKAGES += \
     android.hardware.biometrics.fingerprint@2.3.vendor \
