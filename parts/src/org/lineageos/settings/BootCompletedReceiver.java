a465ad6f52c3fca7473d36065abe90562a5b7bdc
Emanuel <emanuelghub@gmail.com>
2023-09-01 17:09:36 +0200

    [SQUASH] sm8350-common: parts: Import DC Dimming and HBM
    this squash imports DC Dimming and HBM to xiaomi parts and rootdir
    it is adjusted to for sm8350
    (*＞ω＜*)♡
    
    inspired by
    
    HBM commit:
    - https://github.com/Mudit200408/new_device_xiaomi_munch/commit/87eab66cf179679143cb904b118358ed74246299
    
    DC dimming commits:
    
    - https://github.com/Mudit200408/new_device_xiaomi_munch/commit/6db27e28ef6e01ec092f43b20d072f61c61c1a33
    - https://github.com/Mudit200408/new_device_xiaomi_munch/commit/49be770010693b49db1628242b6902ee942c6f42
    - https://github.com/Mudit200408/new_device_xiaomi_munch/commit/1e4393d160d5eef8a51e135fa83e621c50a47b13
    
    Co-authored-by: Arian <39061882+ArianK16a@users.noreply.github.com>
    Co-authored-by: Nauval Rizky <enuma.alrizky@gmail.com>
    Co-authored-by: kamikaonashi <thorschi93@gmx.at>

--- parts/src/org/lineageos/settings/BootCompletedReceiver.java
+++ parts/src/org/lineageos/settings/BootCompletedReceiver.java
@@ -20,22 +20,34 @@ package org.lineageos.settings;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
+import android.os.SystemProperties;
 import android.util.Log;
+import androidx.preference.PreferenceManager;
 
 import org.lineageos.settings.doze.DozeUtils;
 import org.lineageos.settings.thermal.ThermalUtils;
 import org.lineageos.settings.refreshrate.RefreshUtils;
+import org.lineageos.settings.utils.FileUtils;
 
 public class BootCompletedReceiver extends BroadcastReceiver {
     private static final boolean DEBUG = false;
     private static final String TAG = "XiaomiParts";
+    private static final String DC_DIMMING_ENABLE_KEY = "dc_dimming_enable";
+    private static final String DC_DIMMING_NODE = "/sys/devices/platform/soc/soc:qcom,dsi-display-primary/dimlayer_exposure";
 
     @Override
     public void onReceive(final Context context, Intent intent) {
+        SharedPreferences sharedPrefs = PreferenceManager.getDefaultSharedPreferences(context);
+    
         if (DEBUG)
             Log.d(TAG, "Received boot completed intent");
         DozeUtils.onBootCompleted(context);
         ThermalUtils.startService(context);
         RefreshUtils.initialize(context);
+        FileUtils.enableService(context);
+        
+        boolean dcDimmingEnabled = sharedPrefs.getBoolean(DC_DIMMING_ENABLE_KEY, false);
+        FileUtils.writeLine(DC_DIMMING_NODE, dcDimmingEnabled ? "1" : "0");
     }
 }
